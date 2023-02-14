febe11d77c452f8f2bdc264f84f9f1e916c28e8d
gustavo-carrazoni <gustavocarrazoni@hotmail.com>
2023-02-13 22:03:47 -0300

    Atualizando lista de exercícios

---
+++ Módulo I/Semana 01/Semana01_exercicio05/Semana01_exercicio05/Exerc05.cs
@@ -0,0 +1,26 @@
+﻿int qtd;
+int c = 0;
+int d = 0;
+
+Console.WriteLine("Quantos números você gostaria de mostrar? ");
+qtd = int.Parse(Console.ReadLine());
+int[] num = new int[qtd];
+num[0] = 1;
+
+for(int i = 0; i < qtd;i++)
+{
+    if(i == 0)
+    {
+        Console.WriteLine(num[i]);
+        num[i+1] = 1 + num[i];
+    }
+    if(i > 0 && i < qtd-1)
+    {
+        Console.WriteLine(num[i]);
+        num[i+1] = num[i-1] + num[i];
+    }
+    if(i == qtd-1)
+    {
+        Console.WriteLine(num[i]);
+    }
+} 
