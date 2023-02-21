44e46acf07f2d1a5ef6768bcf95272a035068547
gustavo-carrazoni <gustavocarrazoni@hotmail.com>
2023-02-21 18:12:55 -0300

    Atualização exercicios 3 ao 6

--- Módulo I/Semana 02/Semana02/AppBanco.cs
+++ Módulo I/Semana 02/Semana02/AppBanco.cs
@@ -1,28 +1,49 @@
 ﻿using Semana02.Classes;
+int n = 0;
 
-ContaBancaria conta1 = new ContaBancaria();
+    ContaBancaria conta1 = new ContaBancaria();
 
-Console.WriteLine("Bem vindo ao maior banco da América Latina! Por favor, digite os dados da sua conta: ");
-Console.Write("Nome do titular: ");
-conta1.titular = Console.ReadLine();
+    Console.WriteLine("Bem vindo ao maior banco da América Latina! Por favor, digite os dados da sua conta: ");
+    Console.Write("Nome do titular: ");
+    conta1.titular = Console.ReadLine();
 
-Console.Write("\nAgência: ");
-conta1.agencia = int.Parse(Console.ReadLine());
+    Console.Write("\nAgência: ");
+    conta1.agencia = int.Parse(Console.ReadLine());
 
-Console.Write("Número da conta: ");
-conta1.numero = int.Parse(Console.ReadLine());
+    Console.Write("Número da conta: ");
+    conta1.numero = int.Parse(Console.ReadLine());
 
-Console.Write("Tipo da Conta: \nDigite 0 para Conta Corrente ou \nDigite 1 para Conta Poupança\n");
+    Console.Write("Tipo da Conta: \nDigite 0 para Conta Corrente ou \nDigite 1 para Conta Poupança\n");
 
-if(Console.ReadLine() == "0")
-{
-    conta1.tipo = TipoConta.CORRENTE;
-}
-else
-{
-    conta1.tipo = TipoConta.POUPANCA;
-}
+    if(Console.ReadLine() == "0")
+    {
+        conta1.tipo = ContaBancaria.TipoConta.CORRENTE;
+    }
+    else
+    {
+        conta1.tipo = ContaBancaria.TipoConta.POUPANCA;
+    }
 
-Console.WriteLine("Seu nome é " + conta1.titular + " e possui uma Conta " + conta1.tipo + " na agência " + conta1.agencia + ", de número " + conta1.numero + ".");
+    Console.WriteLine("Seu nome é " + conta1.titular + " e possui uma Conta " + conta1.tipo + " na agência " + conta1.agencia + ", de número " + conta1.numero + ".");
 
-Console.Write("\nSeu saldo atual é R$ " + conta1.saldo + ".");
+    do { 
+
+        Console.WriteLine("O que gostaria de fazer?\nSaque - 0\nDepósito - 1");
+        int i = int.Parse(Console.ReadLine());
+        if(i == 0)
+        {
+            Console.WriteLine("Digite o valor do saque desejado: ");
+            float s = float.Parse(Console.ReadLine());
+            conta1.Sacar(s);
+        }
+        if(i == 1)
+        {
+            Console.WriteLine("Digite o valor para depósito: ");
+            float d = float.Parse(Console.ReadLine());
+            conta1.Depositar(d);
+        }
+
+        Console.WriteLine("\nSeu saldo atual é R$ " + conta1.saldo + ".");
+        Console.WriteLine("Deseja realizar outra operação? \nSim - 0 \nNão - 1");
+        n = int.Parse(Console.ReadLine());
+    }while (n != 1) ;
--- Módulo I/Semana 02/Semana02/Classes/ContaBancaria.cs
+++ Módulo I/Semana 02/Semana02/Classes/ContaBancaria.cs
@@ -13,11 +13,41 @@ namespace Semana02.Classes
         public string titular;
         public float saldo = 0;
         public TipoConta tipo;
-    }
 
-    public enum TipoConta
-    {
-        CORRENTE = 0,
-        POUPANCA = 1
+
+        public enum TipoConta
+        {
+            CORRENTE = 0,
+            POUPANCA = 1
+        }
+
+        public void Depositar(float valor)
+        {
+            if (valor <= 0)
+            {
+                Console.WriteLine("O valor para depósito deve ser maior que 0!");
+            }
+            else
+            {
+                saldo = saldo + valor;
+            }
+        }
+
+        public void Sacar(float valor)
+        {
+            if(valor <= 0)
+            {
+                Console.WriteLine("O valor sacado deve ser maior que 0!");
+            }
+            if(valor > saldo)
+            {
+                Console.WriteLine("O valor é maior do que o saldo disponível em conta!");
+            }
+            if(valor > 0 && valor <= saldo)
+            {
+                saldo = saldo - valor;
+                Console.WriteLine("Saque realizado!");
+            }
+        }
     }
 }
